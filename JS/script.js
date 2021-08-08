310d03e0fc954327b7c9e7cae9777d9fc97764b6
Abdel-rhman1 <yousef777906@gmail.com>
2021-08-08 13:43:29 +0200

    adding moveToTop button0

--- JS/script.js
+++ JS/script.js
@@ -4,6 +4,8 @@ var lightBoxItem = document.getElementById("lightBoxItem");
 var close = document.getElementById("close");
 var prev = document.getElementById("prev");
 var next = document.getElementById("next");
+var moveToUp = document.getElementById("moveToUp");
+var navbar = document.getElementById("navbar");
 var currentImg;
 console.log(imgs);
 
@@ -26,7 +28,29 @@ function closeSlid(){
     lightBoxContainer.style.display = "none";
 }
 
-
+document.addEventListener("scroll" , function(){
+    console.log(scrollY);
+    if(scrollY > 200){
+        moveToUp.style.display = "flex";
+    }
+    else{
+        moveToUp.style.display = "none";
+    }
+});
+document.addEventListener("scroll" , function(){
+    console.log("Enter The Two");
+    if(scrollY > 200){
+        
+        navbar.classList.remove("bg-transparent");
+        navbar.style.backgroundColor = "#4e4e4e !important";
+    }else{
+        navbar.classList.add("bg-transparent");
+    }
+});
+moveToUp.addEventListener("click" , function(){
+    console.log("Clicking On its");
+    window.scrollTo(0,0);
+});
 document.body.addEventListener("keydown" , function(evetntInfo){
     console.log(evetntInfo);
     if(evetntInfo.code == "ArrowLeft"){
